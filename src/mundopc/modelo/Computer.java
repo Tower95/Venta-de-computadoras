3c87a6402a01f587a6def8bd0688af2608e013a7
Ismael Torres <torrestmi3@gmail.com>
2024-07-19 00:26:44 -0700

    Add class computer

---
+++ src/mundopc/modelo/Computer.java
@@ -0,0 +1,64 @@
+package mundopc.modelo;
+
+public class Computer {
+
+  // Properties
+  private final int idComputer;
+  private static int computerCounter = 0;
+  private String name;
+  private Monitor monitor;
+  private Keyboard keyboard;
+  private Mouse mouse;
+
+
+  // Constructors
+  private Computer(){
+    this.idComputer = ++computerCounter ;
+  }
+
+  public Computer(String name, Monitor monitor, Keyboard keyboard, Mouse mouse){
+    this();
+    this.name = name;
+    this.monitor = monitor;
+    this.keyboard = keyboard;
+    this.mouse = mouse;
+  }
+
+  // getters and setters
+
+  public String getName() {
+    return name;
+  }
+
+  public Monitor getMonitor() {
+    return monitor;
+  }
+
+  public Keyboard getKeyboard() {
+    return keyboard;
+  }
+
+  public Mouse getMouse() {
+    return mouse;
+  }
+
+  public int getIdComputer() {
+    return idComputer;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setMonitor(Monitor monitor) {
+    this.monitor = monitor;
+  }
+
+  public void setKeyboard(Keyboard keyboard) {
+    this.keyboard = keyboard;
+  }
+
+  public void setMouse(Mouse mouse) {
+    this.mouse = mouse;
+  }
+}
